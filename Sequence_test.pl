98cc551df4ca93596cc0df46bf485dfaf3ad6d2f
David Webber <david.webber@gmail.com>
2013-07-15 22:14:30 -0500

    added some debugging.  Current script is in a debug mode.

--- Sequence_test.pl
+++ Sequence_test.pl
@@ -6,7 +6,7 @@ use Bio::SeqIO;
 $inputfile = "SampleFile.txt";
 
 # you shouldn't have to modify anything below this line
-$debug=0; #set to 1 for some debugging output
+$debug=1; #set to 1 for some debugging output
 
 
 #read in the reference sequence
@@ -19,7 +19,7 @@ foreach $line (@lines) {
     $ref_seq=$line;
   }
 }
-if ($debug==1) {print $ref_seq."\n";}
+if ($debug==2) {print $ref_seq."\n";}
 
 
 #read in the variants
@@ -40,7 +40,7 @@ foreach $line (@lines) {
     $variant_seq[$j++]=$line;
   }
 }
-if ($debug==1){
+if ($debug==2){
   print @variant_names;
   foreach $line (@variant_seq){
     print $line."\n";
@@ -54,29 +54,41 @@ if ($i != $j){
 $seqio_obj = Bio::SeqIO->new(-file => $inputfile, -format => "fasta" );
 
 if ($debug==1){ # for testing 
-  $seq_obj = $seqio_obj->next_seq; 
+  for ($i=0;$i<10;$i++){
+    $seq_obj = $seqio_obj->next_seq;
+  } 
   print $seq_obj->display_id."\n";
   print $seq_obj->desc."\n";
   print $seq_obj->seq."\n";
+  print substr $seq_obj->seq,60+60+27,9;
+  print "\n";
+  print "  ",substr $seq_obj->seq,60+60+29,5;
+  print "\n";
+  print "    ",substr $seq_obj->seq,60+60+31,1;
+  print "\n";
   print "exiting after 1 read\n";
   exit;
 }
 
 # do the tests
-print "display Id,test 239,test 464\n"; #header
+print "display Id,test base 239,test base 464\n"; #header
 
+$offset=88; #normally 88
+$counter=1;
 while ($seq_obj = $seqio_obj->next_seq) {   
+  print $counter,",";
 # print the display_id   
   print $seq_obj->display_id,",";
 
-# test pair 239 (the offset on all reads is 91) 
-  $base239 = substr $seq_obj->seq,239-91,1;
+# test pair 239 (the offset on all reads is 90) 
+  $base239 = substr $seq_obj->seq,239-$offset,9;
 # is this a useful output, or would true/false be better?
   print $base239;
   print ",";
-# test pair 239 (the offset on all reads is 91)
-  $base474 = substr $seq_obj->seq,474-91,1;
+# test pair 239 (the offset on all reads is 90)
+  $base474 = substr $seq_obj->seq,474-$offset,9;
   print $base474;
   print "\n";
+  $counter+=1;
 }
 
