185eb9b1a8187a327ef458b0ba568ac86469e5b3
David Webber <david.webber@gmail.com>
2013-10-29 22:54:53 -0500

    modified MID4->MID04 filename

--- doit.sh
+++ doit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 today=`date +%b%d`
 ./SilentDNA.pl SampleFile.txt   > sample_$today.csv
-./SilentDNA.pl MID4_FA1090.fna  > MID4_FA1090_$today.csv
+./SilentDNA.pl MID04_FA1090.fna > MID04_FA1090_$today.csv
 ./SilentDNA.pl MID24_FA1090.fna > MID24_FA1090_$today.csv
