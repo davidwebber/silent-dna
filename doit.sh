1495b784190241cbc0df4119eb50b0d0057c4631
David Webber <david.webber@gmail.com>
2013-11-05 21:39:00 -0600

    added a new sequence to 7c1 with a single base change from C to G.  Partially addresses issue #8

--- doit.sh
+++ doit.sh
@@ -2,4 +2,5 @@
 today=`date +%b%d`
 ./SilentDNA.pl SampleFile.txt   > sample_$today.csv
 ./SilentDNA.pl MID04_FA1090.fna > MID04_FA1090_$today.csv
+./SilentDNA.pl MID10_FA1090.fna > MID10_FA1090_$today.csv
 ./SilentDNA.pl MID24_FA1090.fna > MID24_FA1090_$today.csv
